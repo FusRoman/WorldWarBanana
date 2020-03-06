4e41bb39ccabda98ea01a8412728b576abc5eb9b
Guilhem Turbiau <gturbia@a-139160.ups.u-psud.fr>
2020-03-06 09:53:36 +0100

    Reorganized the files

---
+++ WorldWarBanana/.depends/custom/Chasseur.d
@@ -0,0 +1,28 @@
+.build/custom/Chasseur.o: src/custom/Chasseur.cc \
+ include/custom/Chasseur.h include/definitive/Mover.h \
+ include/definitive/FireBall.h include/definitive/Environnement.h \
+ include/definitive/Sound.h lib//fmod/4.3/fmod.hpp lib//fmod/4.3/fmod.h \
+ lib//fmod/4.3/fmod_codec.h lib//fmod/4.3/fmod_dsp.h \
+ lib//fmod/4.3/fmod_memoryinfo.h lib//fmod/4.3/fmod_errors.h
+
+include/custom/Chasseur.h:
+
+include/definitive/Mover.h:
+
+include/definitive/FireBall.h:
+
+include/definitive/Environnement.h:
+
+include/definitive/Sound.h:
+
+lib//fmod/4.3/fmod.hpp:
+
+lib//fmod/4.3/fmod.h:
+
+lib//fmod/4.3/fmod_codec.h:
+
+lib//fmod/4.3/fmod_dsp.h:
+
+lib//fmod/4.3/fmod_memoryinfo.h:
+
+lib//fmod/4.3/fmod_errors.h:
---
+++ WorldWarBanana/.depends/custom/Labyrinthe.d
@@ -0,0 +1,36 @@
+.build/custom/Labyrinthe.o: src/custom/Labyrinthe.cc \
+ include/custom/Labyrinthe.h include/definitive/Environnement.h \
+ include/custom/Chasseur.h include/definitive/Mover.h \
+ include/definitive/FireBall.h include/definitive/Environnement.h \
+ include/definitive/Sound.h lib//fmod/4.3/fmod.hpp lib//fmod/4.3/fmod.h \
+ lib//fmod/4.3/fmod_codec.h lib//fmod/4.3/fmod_dsp.h \
+ lib//fmod/4.3/fmod_memoryinfo.h lib//fmod/4.3/fmod_errors.h \
+ include/custom/Gardien.h
+
+include/custom/Labyrinthe.h:
+
+include/definitive/Environnement.h:
+
+include/custom/Chasseur.h:
+
+include/definitive/Mover.h:
+
+include/definitive/FireBall.h:
+
+include/definitive/Environnement.h:
+
+include/definitive/Sound.h:
+
+lib//fmod/4.3/fmod.hpp:
+
+lib//fmod/4.3/fmod.h:
+
+lib//fmod/4.3/fmod_codec.h:
+
+lib//fmod/4.3/fmod_dsp.h:
+
+lib//fmod/4.3/fmod_memoryinfo.h:
+
+lib//fmod/4.3/fmod_errors.h:
+
+include/custom/Gardien.h:
